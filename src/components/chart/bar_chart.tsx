d2d9b34ca9df5021bc7f1445ef1fc17439ecba85
Yunus Emre ATA <yunusemreata@outlook.com>
2023-09-25 15:33:18 +0300

    chart update

--- src/components/chart/bar_chart.tsx
+++ src/components/chart/bar_chart.tsx
@@ -1,66 +1,89 @@
-"use client"
+"use client";
 import React, { Component } from "react";
 import Chart from "react-apexcharts";
-import { any } from "zod";
 
-class App extends Component {
+interface AppState {
+    options: {
+        chart: {
+            id: string;
+        };
+        xaxis: {
+            categories: string[];
+        };
+        colors: string[];
+        plotOptions: {
+            bar: {
+                horizontal: boolean;
+                columnWidth: string;
+                endingShape: string;
+            };
+        };
+        dataLabels: {
+            enabled: boolean;
+            // formatter: (val: any) => string; // Eğer bu işlevi kullanmak isterseniz, tip tanımını güncellemeniz gerekebilir.
+        };
+        stroke: {
+            show: boolean;
+            width: number;
+            colors: string[];
+        };
+    };
+    series: {
+        name: string;
+        data: number[];
+    }[];
+}
 
-    constructor(props) {
+class App extends Component<{}, AppState> {
+    constructor(props: {}) {
         super(props);
 
         this.state = {
             options: {
                 chart: {
-                    id: "basic-bar"
+                    id: "basic-bar",
                 },
                 xaxis: {
-                    categories: ["Pazartesi", "Salı", "Çarşamba", "Perşembe", "Cuma", "Cumartesi", "Pazar"]
+                    categories: ["Pazartesi", "Salı", "Çarşamba", "Perşembe", "Cuma", "Cumartesi", "Pazar"],
                 },
                 colors: ['#7367F0'],
                 plotOptions: {
                     bar: {
                         horizontal: false,
                         columnWidth: '30%',
-                        endingShape: 'rounded'
+                        endingShape: 'rounded',
                     },
-
                 },
                 dataLabels: {
                     enabled: true,
                     // formatter: function (val) {
                     //     return val + "%";
                     // }
-                    
-
                 },
                 stroke: {
                     show: true,
                     width: 2,
-                    colors: ['transparent']
+                    colors: ['transparent'],
                 },
-
             },
             series: [
                 {
                     name: "Etkileşim",
-                    data: [30, 40, 45, 50, 49, 60, 40]
-                }
-            ]
+                    data: [30, 40, 45, 50, 49, 60, 40],
+                },
+            ],
         };
     }
 
     render() {
         return (
-
             <Chart
                 options={this.state.options}
                 series={this.state.series}
                 type="bar"
                 width="100%"
-                height="300"
-
+                height={300}
             />
-
         );
     }
 }
